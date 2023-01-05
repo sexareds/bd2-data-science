37562110c3199ef548873303c4c3cee62049197a
sexareds <cesarrojasv60@gmail.com>
2023-01-05 03:03:07 -0400

    App

--- src/app.py
+++ src/app.py
@@ -1,41 +1,26 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+import sql_scripts
 
-
-# TO DO: find a way to refactor this whole function in order to make it work for the other plots
-# Hint: get the attribute directly from cols 
-# Hint: split the function into two: one for querying and another for plotting
-
-# def show_plot(query: str, cols: list, title: str, xlabel: str, ylabel: str) -> None:
-#     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-#     results.groupby(['Titulo']).size().plot(kind='pie', y='Titulo', autopct='%1.1f%%')
-#     plt.show()
-
-# def query_db(query: str) -> sqlite3.Cursor:
-#     return dat.execute(query)
-
-def get_students_EIU(dat) -> None:
-    query = dat.execute('''
-        SELECT Opcion.titulo AS Titulo
-        FROM Encuesta
-        INNER JOIN Respuesta
-        ON Encuesta.num_encuesta = Respuesta.num_encuesta
-        INNER JOIN Opcion
-        ON Respuesta.num_pregunta = Opcion.num_pregunta
-        AND Respuesta.num_opcion = Opcion.num_opcion
-        AND Opcion.num_pregunta = 4
-    ''')
+def get_sql(dat: sqlite3.Connection, sql_script: str) -> None:
+    query = dat.execute(sql_script)
     cols = [column[0] for column in query.description]
+    return query, cols
+
+def show_plot(query: str, cols: list, title: str, xlabel: str, ylabel: str) -> None:
     results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    print(results)
     results.groupby(['Titulo']).size().plot(kind='pie', y='Titulo', autopct='%1.1f%%')
     plt.show()
+
+def get_students_EIU(dat: sqlite3.Connection) -> None:
+    query, cols = get_sql(dat, sql_scripts.sql_script_1)
+    show_plot(query, cols, 'Estudiantes de la EIU', None, None)
 
 def main() -> None:
     try:
         dat = sqlite3.connect('data/datos.s3db')
+        print(type(dat))
         get_students_EIU(dat)
     except sqlite3.Error as e:
         print(e)
